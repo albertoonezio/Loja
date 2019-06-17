c461c5d76b8aa91e2ba50c7091aa65ebed0a15a0
Alberto <albertomaronezio@hotmai.com>
2019-06-17 17:37:32 -0300

    Add NHibernateHelper

---
+++ Loja/Infra/NHibernateHelper.cs
@@ -0,0 +1,22 @@
+﻿using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Infra
+{
+    public class NHibernateHelper
+    {
+        public static Configuration RecuperaConfiguracao()
+        {
+            Configuration cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+
+            return cfg;
+        }
+    }
+}
