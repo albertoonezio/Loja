e00f9d9eb8099ca1c6f98038a82fc34822100ac3
Alberto <albertomaronezio@hotmai.com>
2019-06-18 12:27:41 -0300

    HQL

--- Loja/Infra/NHibernateHelper.cs
+++ Loja/Infra/NHibernateHelper.cs
@@ -1,4 +1,6 @@
-﻿using NHibernate.Cfg;
+﻿using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +12,14 @@ namespace Loja.Infra
 {
     public class NHibernateHelper
     {
+        private static ISessionFactory fabrica = CriaSessionFactory();
+
+        private static ISessionFactory CriaSessionFactory()
+        {
+            Configuration cfg = RecuperaConfiguracao();
+            return cfg.BuildSessionFactory();
+        }
+
         public static Configuration RecuperaConfiguracao()
         {
             Configuration cfg = new Configuration();
@@ -17,6 +27,17 @@ namespace Loja.Infra
             cfg.AddAssembly(Assembly.GetExecutingAssembly());
 
             return cfg;
+        }
+
+        public static void GeraSchema()
+        {
+            Configuration cfg = RecuperaConfiguracao();
+            new SchemaExport(cfg).Create(true, true);
+        }
+
+        public static ISession AbreSession()
+        {
+            return fabrica.OpenSession();
         }
     }
 }
--- Loja/Program.cs
+++ Loja/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Loja.DAO;
+using Loja.Entidades;
+using Loja.Infra;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Transform;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +16,24 @@ namespace Loja
     {
         static void Main(string[] args)
         {
+            //NHibernateHelper.GeraSchema();
+
+            ISession session = NHibernateHelper.AbreSession();
+
+            String hql = "select p.Categoria as Categoria, count(p) as NumeroDePedidos from Produto p group by p.Categoria";
+            IQuery query = session.CreateQuery(hql);
+            query.SetResultTransformer(Transformers.AliasToBean<ProdutosPorCategoria>());
+
+            IList<ProdutosPorCategoria> relatorio = query.List<ProdutosPorCategoria>();
+            session.Close();
+
+            Console.Read();
         }
+    }
+
+    public class ProdutosPorCategoria
+    {
+        public Categoria Categoria { get; set; }
+        public long NumeroDePedidos { get; set; }
     }
 }
